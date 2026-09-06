75f465bff2ef6a29d8b905bbdba428cca5909e1c
RadwaMohsen222 <64417011+RadwaMohsen222@users.noreply.github.com>
2026-09-06 23:08:07 +0300

    Refactor flipbook options and event handling

--- newsletter1cover.js
+++ newsletter1cover.js
@@ -6,49 +6,6 @@ window.FlipbookSettings = {
         height: 1024,
 
         display: "double",
-
-        page: 1,
-
-        pages: 7,
-
-        autoCenter: true,
-
-        animatedAutoCenter: true,
-
-        duration: 600,
-
-        elevation: "10%",
-
-        gradients: true,
-
-        smartFlip: true,
-
-        swipe: true,
-
-        responsive: true,
-
-        autoScaleContent: true,
-
-        acceleration: true,
-
-        hover: true,
-
-        turnCorners: "l,r",
-
-        pageMargin: "0px 0px"
-
-    }
-$(function () {
-
-    var $flipbook = $("#flipbook");
-
-    var options = {
-
-        width: 932,
-        height: 1024,
-
-        display: "double",
-
         page: 1,
         pages: 7,
 
@@ -69,52 +26,25 @@ $(function () {
         hover: true,
 
         turnCorners: "l,r",
+        pageMargin: "0px 0px",
 
         when: {
 
             turned: function (event, page) {
 
-                /*
-                 * When the back cover is reached,
-                 * wait 2 seconds and restart.
-                 */
+                // When the back cover is reached,
+                // wait 2 seconds and restart from the front.
 
                 if (page === 7) {
 
                     setTimeout(function () {
 
-                        $flipbook.turn("page", 1);
+                        $("#flipbook").turn("page", 1);
 
                     }, 2000);
 
                 }
-
             }
-
         }
-
-    };
-
-
-    $flipbook.turn(options);
-
-
-    /*
-     * NEXT ARROW
-     */
-    $(".ui-arrow-next-page").on("click", function () {
-
-        $flipbook.turn("next");
-
-    });
-
-
-    /*
-     * PREVIOUS ARROW
-     */
-    $(".ui-arrow-previous-page").on("click", function () {
-
-        $flipbook.turn("previous");
-
-    });
+    }
 };
