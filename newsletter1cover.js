7167d87be002a09666997ec2467bcfe173aad459
RadwaMohsen222 <64417011+RadwaMohsen222@users.noreply.github.com>
2026-09-06 23:28:18 +0300

    Refactor flipbook initialization in newsletter1cover.js
    Removed unnecessary code and simplified flipbook initialization.

--- newsletter1cover.js
+++ newsletter1cover.js
@@ -1,93 +1,11 @@
 $(document).ready(function () {
-
-    var flipbook = $("#flipbook");
-
-
-    /* =========================
-       START FLIPBOOK
-    ========================= */
-
-    flipbook.turn({
-
-        width: 932,
-        height: 1024,
-
-        display: "double",
-
-        pages: 7,
-
-        page: 1,
-
-        duration: 600,
-
-        autoCenter: true,
-
-        gradients: true,
-
-        elevation: 10,
-
-        acceleration: true,
-
-        direction: "ltr",
-
-        when: {
-
-            turning: function (event, page) {
-
-                console.log("Turning to page:", page);
-
-            },
-
-            turned: function (event, page) {
-
-                console.log("Currently on page:", page);
-
-
-                /*
-                 * BACK COVER
-                 *
-                 * Page 7 is the back cover.
-                 *
-                 * Wait 2 seconds,
-                 * then return to page 1.
-                 */
-
-                if (page === 7) {
-
-                    setTimeout(function () {
-
-                        flipbook.turn("page", 1);
-
-                    }, 2000);
-
-                }
-
-            }
-
-        }
-
-    });
-
-
-    /* =========================
-       NEXT BUTTON
-    ========================= */
-
-    $("#next").click(function () {
-
-        flipbook.turn("next");
-
-    });
-
-
-    /* =========================
-       PREVIOUS BUTTON
-    ========================= */
-
-    $("#previous").click(function () {
-
-        flipbook.turn("previous");
-
-    });
-
+  $("#flipbook").turn({
+    width: 932,
+    height: 1024,
+    autoCenter: true,
+    duration: 1000,
+    acceleration: true,
+    gradients: true,
+    elevation: 50
+  });
 });
