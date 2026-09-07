67bcb6f1111b0a9ab9eb729adc0e5b63580904bd
RadwaMohsen222 <64417011+RadwaMohsen222@users.noreply.github.com>
2026-09-07 06:30:01 +0300

    Disable gradients and set elevation to zero

--- script.js
+++ script.js
@@ -40,8 +40,8 @@ $(document).ready(function() {
                 height: config.height,
                 display: config.display,
                 autoCenter: true, // Centers page 1 horizontally
-                gradients: true,
-                elevation: 50
+                gradients: false, // Disabled to eliminate animated page shadows
+                elevation: 0      // Set to 0 to remove 3D lift shadows
             });
             $flipbook.data().done = true;
         } else {
