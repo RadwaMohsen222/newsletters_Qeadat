79cb0d3ca8cc5d91ca2a7b65cb4d0a7037eade1a
RadwaMohsen222 <64417011+RadwaMohsen222@users.noreply.github.com>
2026-09-07 03:55:50 +0300

    Refactor flipbook initialization and hover effect

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
-    $("#flipbook").turn({
-        // Dimensions scaled down by exactly half
+    var $flipbook = $("#flipbook");
+
+    $flipbook.turn({
         width: 466, 
         height: 512,
         autoCenter: true, 
         display: 'double',
         gradients: true, 
         elevation: 50 
+    });
+
+    // Add the hover effect class initially since the book starts closed
+    $flipbook.addClass('is-closed');
+
+    // Listen for page turns to disable the hover effect when reading
+    $flipbook.bind("turning", function(event, page, view) {
+        if (page === 1) {
+            // Book is back on the front cover, turn hover back on
+            $flipbook.addClass('is-closed');
+        } else {
+            // Book is open, turn hover off
+            $flipbook.removeClass('is-closed');
+        }
     });
 });
