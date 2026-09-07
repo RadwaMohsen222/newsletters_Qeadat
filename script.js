46e1f45c560ba974eab2bcefdea484d5620c791e
RadwaMohsen222 <64417011+RadwaMohsen222@users.noreply.github.com>
2026-09-07 04:01:52 +0300

    Clarify autoCenter option in flipbook initialization
    Added comment to clarify autoCenter functionality.

--- script.js
+++ script.js
@@ -4,22 +4,18 @@ $(document).ready(function() {
     $flipbook.turn({
         width: 466, 
         height: 512,
-        autoCenter: true, 
+        autoCenter: true, // This shifts the book to the middle when on page 1
         display: 'double',
         gradients: true, 
         elevation: 50 
     });
 
-    // Add the hover effect class initially since the book starts closed
     $flipbook.addClass('is-closed');
 
-    // Listen for page turns to disable the hover effect when reading
     $flipbook.bind("turning", function(event, page, view) {
         if (page === 1) {
-            // Book is back on the front cover, turn hover back on
             $flipbook.addClass('is-closed');
         } else {
-            // Book is open, turn hover off
             $flipbook.removeClass('is-closed');
         }
     });
