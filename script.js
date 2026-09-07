fb3cb39f3655b4bdc82dc556fbf5ee6e4fda93ca
RadwaMohsen222 <64417011+RadwaMohsen222@users.noreply.github.com>
2026-09-07 04:07:08 +0300

    Refactor flipbook initialization and event handling

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 $(document).ready(function() {
-    var $flipbook = $("#flipbook");
-
-    $flipbook.turn({
+    $("#flipbook").turn({
         width: 466, 
         height: 512,
-        autoCenter: true, // This shifts the book to the middle when on page 1
+        autoCenter: true, 
         display: 'double',
         gradients: true, 
         elevation: 50 
-    });
-
-    $flipbook.addClass('is-closed');
-
-    $flipbook.bind("turning", function(event, page, view) {
-        if (page === 1) {
-            $flipbook.addClass('is-closed');
-        } else {
-            $flipbook.removeClass('is-closed');
-        }
     });
 });
